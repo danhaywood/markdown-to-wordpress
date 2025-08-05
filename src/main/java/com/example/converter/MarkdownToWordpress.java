8f69fe3d822f4d12a35222a6e630dd12cf0cbedf
Dan Haywood <dan@haywood-associates.co.uk>
2025-08-05 15:24:51 +0300

    refactors to use Converter interface

--- src/main/java/com/example/converter/MarkdownToWordpress.java
+++ src/main/java/com/example/converter/MarkdownToWordpress.java
@@ -1,16 +1,21 @@
 
 package com.example.converter;
 
+import com.vladsch.flexmark.ast.Heading;
+import com.vladsch.flexmark.ast.Paragraph;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
 import com.vladsch.flexmark.util.data.MutableDataSet;
 
+import lombok.RequiredArgsConstructor;
 import picocli.CommandLine;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 
 import org.jetbrains.annotations.NotNull;
@@ -52,45 +57,80 @@ public class MarkdownToWordpress implements Callable<Integer> {
 
         node.getChildren()
                 .forEach(child -> {
-            final var markdownHtml = renderer.render(child).trim();
-            for (Converter converter : converters) {
-                final var convertedHtml = converter.convert(markdownHtml);
-                if (convertedHtml != null) {
-                    buf.append(convertedHtml);
-                    break;
-                }
-            }
+                    converters.stream()
+                            .filter(converter -> converter.supports(child))
+                            .map(converter -> converter.convert(renderer, child)).filter(Objects::nonNull)
+                            .findFirst()
+                            .ifPresent(buf::append);
         });
         return buf.toString();
     }
 
     interface Converter {
-        String convert(String html);
+        boolean supports(Node node);
+        String convert(HtmlRenderer renderer, Node node);
+    }
+    @RequiredArgsConstructor
+    abstract static class AbstractConverter implements Converter {
+        final Class<? extends Node> nodeClass;
+
+        @Override
+        public boolean supports(Node node) {
+            return nodeClass.isAssignableFrom(node.getClass());
+        }
+        @Override
+        public String convert(HtmlRenderer renderer, Node node) {
+            final var markdownHtml = renderer.render(node).trim();
+            return doConvert(markdownHtml);
+        }
+
+        protected abstract String doConvert(String markdownHtml);
+
     }
 
     final List<Converter> converters = new ArrayList<>() {{
-        add(html -> {
-            if (html.startsWith("<p>") && html.endsWith("</p>")) {
+        add(new AbstractConverter(Paragraph.class) {
+            @Override
+            protected String doConvert(String markdownHtml) {
                 return
-                    """
-                    <!-- wp:paragraph {"canvasClassName":"cnvs-block-core-paragraph-%s"} -->
-                    %s
-                    <!-- /wp:paragraph -->
-                    """.formatted(timestamp(), html);
+                        """
+                        <!-- wp:paragraph {"canvasClassName":"cnvs-block-core-paragraph-%s"} -->
+                        %s
+                        <!-- /wp:paragraph -->
+                        """.formatted(timestamp(), markdownHtml);
             }
-            return null;
         });
-        add(html -> {
-            if (html.startsWith("<h2>") && html.endsWith("</h2>")) {
+        // h1
+        add(new AbstractConverter(Heading.class) {
+
+            @Override
+            public boolean supports(Node node) {
+                return super.supports(node) && ((Heading) node).getLevel() == 1;
+            }
+
+            @Override
+            protected String doConvert(String markdownHtml) {
+                return "";
+            }
+        });
+        // h2
+        add(new AbstractConverter(Heading.class) {
+
+            @Override
+            public boolean supports(Node node) {
+                return super.supports(node) && ((Heading) node).getLevel() == 2;
+            }
+
+            @Override
+            protected String doConvert(String markdownHtml) {
                 return
                         """
                         <!-- wp:heading {"canvasClassName":"cnvs-block-core-heading-%s"} -->
                         %s
                         <!-- /wp:heading -->
-                        """.formatted(timestamp(), html)
-                                .replaceAll("<h2>", "<h2 class=\"wp-block-heading\">");
+                        """.formatted(timestamp(), markdownHtml)
+                        .replaceAll("<h2>", "<h2 class=\"wp-block-heading\">");
             }
-            return null;
         });
     }};
 
